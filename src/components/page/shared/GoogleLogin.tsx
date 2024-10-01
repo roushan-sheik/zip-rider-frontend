8f761bd99f288c318cba49b4047e0f552c3d2d83
roushan-sheik <roushansheik@gmail.com>
2024-10-01 18:19:20 +0600

    update: efficient role based protecting route

--- src/components/page/shared/GoogleLogin.tsx
+++ src/components/page/shared/GoogleLogin.tsx
@@ -3,11 +3,17 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 import { Button } from "@nextui-org/react"
 import React from "react"
+import { useSearchParams } from "next/navigation"
 
 const GoogleLogin = () => {
+  const searchParams = useSearchParams()
+  const redirect = searchParams.get("redirect")
+
   return (
     <Button
-      onClick={() => signIn("google", { callbackUrl: "/" })}
+      onClick={() =>
+        signIn("google", { callbackUrl: redirect ? redirect : "/" })
+      }
       variant="ghost"
       color="danger"
     >
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,17 @@ import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 import { decodeToken } from "./helpers"
-import { ROLES } from "./enum"
 
 // auth routes
 const authRoutes = ["/login", "register"]
+// role base route
+type TRole = keyof typeof RoleBasedRoute
+
+const RoleBasedRoute = {
+  admin: [/^\/admin-dashboard/],
+  driver: [/^\/driver-dashboard/],
+  user: [/^\/dashboard/],
+}
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
@@ -19,21 +26,25 @@ export function middleware(request: NextRequest) {
       return NextResponse.next()
     } else {
       console.log("accessToken nai amar vai>>>>", accessToken)
-      return NextResponse.redirect(new URL("/login", request.url))
+      return NextResponse.redirect(
+        new URL(
+          pathname ? `/login?redirect=${pathname}` : "/login",
+          request.url
+        )
+      )
     }
   }
   // role based authorization ==========================>
   let decodedToken = null
   decodedToken = decodeToken(accessToken as string) as any
-
   const role = decodedToken?.role
 
-  if (role === ROLES.ADMIN && pathname === "/admin-dashboard") {
-    return NextResponse.next()
-  } else if (role === ROLES.DRIVER && pathname === "/driver-dashboard") {
-    return NextResponse.next()
-  } else if (role === ROLES.USER && pathname === "/dashboard") {
-    return NextResponse.next()
+  if (role && RoleBasedRoute[role as TRole]) {
+    const routes = RoleBasedRoute[role as TRole]
+
+    if (routes.some((route: any) => pathname.match(route))) {
+      return NextResponse.next()
+    }
   }
 
   return NextResponse.redirect(new URL("/", request.url))
