5dd8cc1339accca8c597a867148f16958b34a917
roushan-sheik <roushansheik@gmail.com>
2024-10-01 09:40:05 +0600

    update : organize next auth options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,47 +1,6 @@
 import { authOptions } from "@/utils/authOptions"
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import config from "./../../../../config/env.config"
-import nexiosInstance from "@/config/nexios.config"
-import { NextResponse } from "next/server"
-import { cookies } from "next/headers"
 
-const handler = NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: config.google_client as string,
-      clientSecret: config.google_secret as string,
-    }),
-  ],
-
-  callbacks: {
-    // google callback ===========================>
-    async signIn({ account, profile }: any) {
-      if (!account && !profile) {
-        return false
-      }
-      if (account?.provider === "google") {
-        // create user into db
-        const response: any = await nexiosInstance.post("/auth/login", {
-          name: profile?.name,
-          email: profile?.email,
-          img: profile?.picture,
-        })
-        // set accessToken into browser cookie
-        if (response.data.data.accessToken && response.data.data.refreshToken) {
-          cookies().set("accessToken", response.data.data.accessToken)
-          cookies().set("refreshToken", response.data.data.refreshToken)
-        }
-      }
-      return true // Do different verification for other providers that don't have `email_verified`
-    },
-  },
-  pages: {
-    signIn: "/login",
-  },
-  secret: config.next_auth_secret,
-})
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,14 +1,44 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import config from "@/config/env.config"
+import nexiosInstance from "@/config/nexios.config"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-export const authOptions = {
+import { cookies } from "next/headers"
+
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: config.google_client as string,
+      clientSecret: config.google_secret as string,
     }),
   ],
+
+  callbacks: {
+    // google callback ===========================>
+    async signIn({ account, profile }: any) {
+      if (!account && !profile) {
+        return false
+      }
+      if (account?.provider === "google") {
+        // create user into db
+        const response: any = await nexiosInstance.post("/auth/login", {
+          name: profile?.name,
+          email: profile?.email,
+          img: profile?.picture,
+        })
+        // set accessToken into browser cookie
+        if (response.data.data.accessToken && response.data.data.refreshToken) {
+          cookies().set("accessToken", response.data.data.accessToken)
+          cookies().set("refreshToken", response.data.data.refreshToken)
+        }
+      }
+      return true // Do different verification for other providers that don't have `email_verified`
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+  secret: config.next_auth_secret,
 }
 
 export default NextAuth(authOptions)
