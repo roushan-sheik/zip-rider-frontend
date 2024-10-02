193e434f3a15a10b3d398b18d3916e4ed72eccb7
roushan-sheik <roushansheik@gmail.com>
2024-10-02 11:08:10 +0600

    add: user dashboard

--- src/app/(dashboardLayout)/(userDashboard)/dashboard/_layout/userLayout.tsx
+++ src/app/(dashboardLayout)/(userDashboard)/dashboard/_layout/userLayout.tsx
@@ -1,12 +1,13 @@
 "use client"
 
+import { SidebarWrapper } from "@/components/sidebar/UserSidebar"
 import React, { ReactNode } from "react"
 
 const UserLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   console.log("Log inside layout client")
   return (
     <section className="flex">
-      <h2>Side</h2>
+      <SidebarWrapper></SidebarWrapper>
       <div>{children}</div>
       <h2>Nav</h2>
     </section>
